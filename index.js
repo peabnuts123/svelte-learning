89d332cb0e87f82c51a0827e2051bcb38366890b
peabnuts123 <peabnuts123@gmail.com>
2017-02-03 23:14:32 +1300

    Adding base project

---
+++ index.js
@@ -0,0 +1,6 @@
+var testInstance = new BasicModule({
+    target: document.querySelector("#testMountPoint"),
+    data: {
+        testMessage: "Hello world, I am the basic module test message"
+    }
+});
